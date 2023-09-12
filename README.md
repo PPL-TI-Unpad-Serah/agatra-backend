0408f77475080664256a9bb3f83bfd43932d3844
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-09-12 17:31:06 +0700

    coffeeboosted afternoon

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 This is backend
+
+sebelum jalanin, go mod tidy
+
+
+Update Log
+(+) New
+(-) Delete
+(++) Update add minor
+(--) Update delete minor
+
+v0.1 (init?)
+- (+) Main.go
+- (+) go module :  agatra (Arcade GAme TRAcker)
+    - (+) fiber v2
+    - (+) godotenv
+
+v0.2 (coffeeboosted afternoon)
+- (+) Folder api, handlers, repos, utils [all empty]
+- (+) Folder db > (+) postgres.go
+        - (+) struct config, dbstruct
+        - (+) func connect, reset, automigrate [empty]
+- (+) Folder middleware > (+) auth.go > (+) func auth
+- (+) Folder models 
+    - (+) jwt.go > (+) struct claims
+    - (+) response.go
+        - (+) struct Error + Success response
+        - (+) func new error + success response
---
+++ db/postgres.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"fmt"
+	"agatra/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct{
+	Host		string
+	Port		string
+	Password	string
+	User		string
+	DBName		string
+	SSLMode		string
+}
+
+type Dbstruct struct {
+	Db *gorm.DB
+}
+
+var DB Dbstruct
+
+func MigrateModels(db *gorm.DB) {
+	// db.AutoMigrate(models)
+}
+
+func Connect(config *Config) (*gorm.DB, error){
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Jakarta", config.Host, config.User, config.Password, config.DBName, config.Port)
+
+	dbConn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn, nil 
+}
+
+func Reset(db *gorm.DB, table string) error {
+	return db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Exec("TRUNCATE " + table).Error; err != nil {
+			return err
+		}
+
+		if err := tx.Exec("ALTER SEQUENCE " + table + "_id_seq RESTART WITH 1").Error; err != nil {
+			return err
+		}
+	})
+}

ff61d1caf4291717dde56b5809410bf6b958f222
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-11-24 17:37:27 +0700

    maintainer

--- middleware/auth.go
+++ middleware/auth.go
@@ -73,6 +73,48 @@ func AuthAdmin(db *gorm.DB) gin.HandlerFunc {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Trouble finding user"})
 		}
 
+		if compare.Role == "admin"{
+			ctx.Set("email", claims.Email)
+			ctx.Next()
+		}else{
+			ctx.AbortWithStatusJSON(403, gin.H{"error": "Insufficient Permission"})
+		}
+	})
+}
+
+func AuthMaintainer(db *gorm.DB) gin.HandlerFunc {
+	return gin.HandlerFunc(func(ctx *gin.Context) {
+		data, err := ctx.Cookie("session_token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				if ctx.GetHeader("Content-Type") == "application/json" {
+					ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+				} else {
+					ctx.Redirect(http.StatusSeeOther, "/user/login")
+					ctx.Abort()
+				}
+				return
+			}
+			ctx.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		claims := &model.Claims{}
+
+		tkn, err := jwt.ParseWithClaims(data, claims, func(token *jwt.Token) (interface{}, error) {
+            return model.JwtKey, nil
+        })
+        if err != nil || !tkn.Valid {
+            ctx.JSON(400, model.ErrorResponse{Error: "ga valid bang"})
+            return
+        }
+		
+		var compare model.User
+		err = db.Where("email = ?", claims.Email).First(&compare).Error
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Trouble finding user"})
+		}
+
 		if compare.Role == "member"{
 			ctx.AbortWithStatusJSON(403, gin.H{"error": "Insufficient Permission"})
 		}else{

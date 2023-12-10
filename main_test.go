2fbe2ec2b2718c2b0a09e1fedd21b18844e4a464
OsmondHaylim <osmond20001@mail.unpad.ac.id>
2023-12-10 22:37:06 +0700

    hate this

--- main_test.go
+++ main_test.go
@@ -1,25 +1,28 @@
-package main_test
+package main
 
 import (
-	"agatra/controller"
 	"agatra/db"
 	"agatra/model"
-	"testing"
-	"net/http"
-	"net/http/httptest"
 
 	_ "embed"
 	"fmt"
+	"sync"
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"time"
+	"strings"
+	"io"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 )
 
-func start() *gin.Engine{
+func start() (*gin.Engine, *sync.WaitGroup){
 	gin.SetMode(gin.ReleaseMode)
 	config := &db.Config{
-		Host:     "https://kkmkegheitaqvhygnixh.supabase.co",
+		Host:     "db.kkmkegheitaqvhygnixh.supabase.co",
 		Port:     "5432",
 		Password: "kalomaugenerikaja",
 		User:     "postgres",
@@ -34,22 +37,54 @@ func start() *gin.Engine{
 	conn.AutoMigrate(&model.Version{})
 	conn.AutoMigrate(&model.Machine{}) 
 	conn.AutoMigrate(&model.Location{})
-	router = controller.RunServer(conn, router)
-	fmt.Println("Server is running on port 8080")
-	err = router.Run(":8080")
-	if err != nil {
-		panic(err)
-	}
 
-	return router
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		router = RunServer(conn, router)
+		fmt.Println("Server is running on port 8080")
+		err := router.Run(":8080")
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	return router, &wg
+}
+
+func stop(router *gin.Engine, wg *sync.WaitGroup){
+	wg.Done()
 }
 
 func TestMain(t *testing.T){
-	t.Run("ServerRunning", func(t *testing.T) {
-		req, _ := http.NewRequest("GET", "/arcade_locations", nil)
+	t.Run("Register", func(t *testing.T) {
+		userBodu := 
+		req, _ := http.NewRequest("POST", "http://localhost:8080/agatra/register", strings.NewReader(`{
+			"username" : "Rommel22w",
+			"email" : "rommela.malik@gmail.com",
+			"password" : "abcd",
+			"confirm_password" : "abcd"
+		 }`))
 		w := httptest.NewRecorder()
-		router := start() 
+		router, wg := start() 
+		defer stop(router, wg)
+		
+
+		time.Sleep(10 * time.Second)
+		fmt.Println("Registered Routes:", router.Routes())
 		router.ServeHTTP(w, req)
+		// Send the request to the server
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatalf("failed to send request: %v", err)
+		}
+		defer res.Body.Close()
+
+		// Print the response body for debugging
+		body, _ := io.ReadAll(res.Body)
+		fmt.Println("Response Body:", string(body))
 		assert.Equal(t, http.StatusOK, w.Code)
 	})
 }
